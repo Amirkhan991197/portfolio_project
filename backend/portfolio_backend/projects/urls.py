e0045784fc0c4ff2eea481af99c173ce1a87a65c
Amirkhan991197 <khanamir991197@gmail.com>
2025-09-07 13:38:40 +0530

    Fix settings, media and urls for React + Django integration

--- backend/portfolio_backend/projects/urls.py
+++ backend/portfolio_backend/projects/urls.py
@@ -3,6 +3,8 @@ from rest_framework.routers import DefaultRouter
 from .views import ProjectViewSet
 from django.urls import path, include
 from .views import CertificationViewSet
+from django.conf import settings
+from django.conf.urls.static import static
 
 router = DefaultRouter()
 router.register(r'projects', ProjectViewSet, basename='projects')
@@ -12,3 +14,6 @@ urlpatterns = [
     path('', include(router.urls)),
     path("api/", include(router.urls)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
